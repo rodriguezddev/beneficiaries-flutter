88be6f6f45dc3e8845188e962bcca8a96ab31caa
Daniela Rodriguez <63770522+rodriguezddev@users.noreply.github.com>
2022-09-28 20:22:25 -0400

    feat: add constant and spacings files (#13)

---
+++ lib/src/core/constants/spacings.dart
@@ -0,0 +1,7 @@
+class Spacings {
+  static const double spacing02 = 20;
+  static const double spacing03 = 30;
+  static const double spacing04 = 40;
+  static const double spacing05 = 50;
+  static const double spacing06 = 60;
+}
